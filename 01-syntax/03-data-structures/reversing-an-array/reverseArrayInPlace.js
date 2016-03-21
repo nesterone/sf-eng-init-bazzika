31f981826ba9722feea194ff5a50cf6cb3fe51a5
nesterone <igornester@gmail.com>
2016-03-21 10:30:43 +0200

    -fix data structure

--- 01-syntax/03-data-structures/reversing-an-array/reverseArrayInPlace.js
+++ 01-syntax/03-data-structures/reversing-an-array/reverseArrayInPlace.js
@@ -2,10 +2,12 @@
  *  [Reversing and array](file:///projects/Eloquent-JavaScript/html/04_data.html#h_6xTmjj4Rf5)
  *
  */
-
-// Your code here.
+/* global reverseArrayInPlace */
 
 var arrayValue = [1, 2, 3, 4, 5];
+
+// → Your code here.
+
 reverseArrayInPlace(arrayValue);
 console.log(arrayValue);
 // → [5, 4, 3, 2, 1]
