05771bead9394164b3dead30fd62f43ddca00f43
Artem Konovalov <artem.konovalov@globallogic.com>
2016-03-25 18:58:33 +0200

    Feat: Add Exercise: Fix code styling issues from the section JS_Intro

--- 01-syntax/00-intro/codeStyle.js
+++ 01-syntax/00-intro/codeStyle.js
@@ -1,1 +1,12 @@
-// Your code here.
+var foo = 1;
+var bar = foo;
+var array = [1, 3, 3];
+var i;
+
+if (bar !== undefined && bar !== null) {
+  console.log(foo, bar);
+}
+
+for (i = 0; i < array.length; i++) {
+  console.log(array[i]);
+}
