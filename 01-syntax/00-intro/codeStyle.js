9b20ea38287e2924805f303c623c65af43b5f553
Artem Konovalov <artem.konovalov@globallogic.com>
2016-03-26 17:11:02 +0200

    Fix: Delete previous wrong data

--- 01-syntax/00-intro/codeStyle.js
+++ 01-syntax/00-intro/codeStyle.js
@@ -1,12 +1,1 @@
-var foo = 1;
-var bar = foo;
-var array = [1, 3, 3];
-var i;
-
-if (bar !== undefined && bar !== null) {
-  console.log(foo, bar);
-}
-
-for (i = 0; i < array.length; i++) {
-  console.log(array[i]);
-}
+// Your code here.
