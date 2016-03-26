33558e1114a5ba0176048bec3f7147824634b835
bazzika <daryabelob@gmail.com>
2016-03-26 14:55:15 +0200

    fix #161

--- 01-syntax/00-intro/codeStyle.js
+++ 01-syntax/00-intro/codeStyle.js
@@ -1,1 +1,8 @@
-// Your code here.
+var foo = 1;
+var bar = foo;
+var i = 0;
+var array = [1, 3, 3];
+if (bar) console.log(foo, bar);
+for (i = 0; i < array.length; i++) {
+  console.log(array[i]);
+}
