7c6e0cb5ec45f79d7db9e32d18d1aa5857549442
bazzika <daryabelob@gmail.com>
2016-03-29 15:48:15 +0300

    - print chess board an 8*8 grid

--- 01-syntax/01-language-basics/chessBoard.js
+++ 01-syntax/01-language-basics/chessBoard.js
@@ -1,1 +1,14 @@
-// Your code here
+var line = "";
+for (var i = 0; i < 8; i++) {
+  for (var j = 0; j < 8; j++) {
+    if ((i+j) % 2 == 0) {
+      line += "#";
+    } else {
+      line += " ";
+  }}
+  line += "\n";
+}
+console.log(line);
+
+
+
