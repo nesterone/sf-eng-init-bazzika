da043bbec2390856afd494c6d8d51ccc9713fac4
bazzika <daryabelob@gmail.com>
2016-03-29 18:52:24 +0300

    - draw figure Pyramid

--- 01-syntax/01-language-basics/pyramid.js
+++ 01-syntax/01-language-basics/pyramid.js
@@ -1,1 +1,15 @@
-// Your code here
+var pyramid = "";
+scale = 30;
+for (var i = 0; i <= scale/2 ; i++ ) {
+  for (var j = 0; j <= scale; j++){
+    if ((j > (scale/2-i))&&(j<(scale/2+i))){
+      pyramid += "#";
+    } else {
+      pyramid += " ";
+    }
+  }
+  console.log(pyramid);
+  pyramid = "";
+  }
+
+
