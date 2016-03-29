e3b0f90de635c375e3cca751bd0c07ea7b7c76e8
DeuceX <braga.dmitriy@gmail.com>
2016-03-29 10:16:10 +0300

    Finished: poetry exercise

--- 01-syntax/01-language-basics/poetry.js
+++ 01-syntax/01-language-basics/poetry.js
@@ -46,3 +46,36 @@ Take care about tabs, newlines etc.
 */
 
 // Your code here
+console.log('William Blake. 1757–1827');
+
+console.log('The Tiger');
+
+console.log('GER, tiger, burning bright');
+console.log(' the forests of the night,');
+console.log('    What immortal hand or eye');
+console.log('uld frame thy fearful symmetry?');
+console.log('');
+console.log('    In what distant deeps or skies');
+console.log('rnt the fire of thine eyes?');
+console.log('    On what wings dare he aspire?');
+console.log('    What the hand dare seize the fire?');
+console.log('');
+console.log('    And what shoulder and what art');
+console.log('uld twist the sinews of thy heart?');
+console.log('    And when thy heart began to beat,');
+console.log('    What dread hand and what dread feet?');
+console.log('');
+console.log('    What the hammer? what the chain?');
+console.log('    In what furnace was thy brain?');
+console.log('    What the anvil? What dread grasp');
+console.log('re its deadly terrors clasp?');
+console.log('');
+console.log('    When the stars threw down their spears,');
+console.log('   And water\'d heaven with their tears,');
+console.log('d He smile His work to see?');
+console.log('    Did He who made the lamb make thee?');
+console.log('');
+console.log('    Tiger, tiger, burning bright');
+console.log(' the forests of the night,');
+console.log('    What immortal hand or eye');
+console.log('re frame thy fearful symmetry?');
