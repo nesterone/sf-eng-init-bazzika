5432ec5c66732435bbcfef99eee4a16e83b2fcd1
bazzika <daryabelob@gmail.com>
2016-03-29 13:35:05 +0300

    - add print "FizzBuzz", (2-st version)

--- 01-syntax/01-language-basics/fizzBuzz.js
+++ 01-syntax/01-language-basics/fizzBuzz.js
@@ -1,12 +1,15 @@
+var number;
 for ( var i = 1; i <= 100; i++){
   if ( i%3 == 0 && i%5 == 0){
-    console.log("FizzBuzz");
+    number="FizzBuzz";
   } else if ( i%3 == 0 ){
-    console.log("Fizz");
+    number="Fizz";
   } else if ( i%5 == 0 ){
-    console.log("Buzz");
+    number="Buzz";
   } else {
-    console.log(i);
+    number=i;
   }
+  console.log(number);
 }
+
 
