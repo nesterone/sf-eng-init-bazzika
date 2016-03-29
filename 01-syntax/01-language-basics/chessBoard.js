560c2d53059e657a4c40e8edabd3f0bab243a29c
DeuceX <braga.dmitriy@gmail.com>
2016-03-29 15:48:39 +0300

    Implement 8x8 grid

--- 01-syntax/01-language-basics/chessBoard.js
+++ 01-syntax/01-language-basics/chessBoard.js
@@ -1,1 +1,11 @@
 // Your code here
+var board = ' # # # #\n'
+  + '# # # # \n'
+  + ' # # # #\n'
+  + '# # # # \n'
+  + ' # # # #\n'
+  + '# # # # \n'
+  + ' # # # #\n'
+  + '# # # # #';
+
+console.log(board);
