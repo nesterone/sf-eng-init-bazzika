cb10ee2e5cf04e48a05be86ce01fc42e7a8eb639
bazzika <daryabelob@gmail.com>
2016-03-29 13:09:37 +0300

    - print "Fizz" instead of the number, and for numbers divisible by 5 (and not 3), print "Buzz" instead

--- 01-syntax/01-language-basics/fizzBuzz.js
+++ 01-syntax/01-language-basics/fizzBuzz.js
@@ -1,1 +1,10 @@
-// Your code here
+for (var i=1; i <= 100 ; i++){
+  if ( i%3 == 0){
+    console.log("Fizz");
+  } else if ( i%5 == 0 ){
+    console.log("Buzz");
+  } else {
+    console.log(i);
+  }
+}
+
