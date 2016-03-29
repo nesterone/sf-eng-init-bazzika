2bf29e6aa3b6f7f3f268bc2f78a534c2d39ab3a1
DeuceX <braga.dmitriy@gmail.com>
2016-03-29 15:57:30 +0300

    Implemented grid for any size

--- 01-syntax/01-language-basics/chessBoard.js
+++ 01-syntax/01-language-basics/chessBoard.js
@@ -1,11 +1,19 @@
 // Your code here
-var board = ' # # # #\n'
-  + '# # # # \n'
-  + ' # # # #\n'
-  + '# # # # \n'
-  + ' # # # #\n'
-  + '# # # # \n'
-  + ' # # # #\n'
-  + '# # # # #';
+var size = 8;
+var board = '';
+var i;
+var j;
 
-console.log(board);
+for (j = 0; j < size; j++) {
+  if (j % 2 === 0) {
+    board += ' ';
+  }
+  for (i = 0; i < size; i++) {
+    if (i % 2 === 0) {
+      board += '#';
+    } else {
+      board += ' ';
+    }
+  }
+  board += '\n';
+}
