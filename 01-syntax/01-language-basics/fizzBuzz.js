0986e3120682b7de6c383745884a1d2a2e9b2509
DeuceX <braga.dmitriy@gmail.com>
2016-03-30 13:47:49 +0300

    Implement FizzBuzz exercise

--- 01-syntax/01-language-basics/fizzBuzz.js
+++ 01-syntax/01-language-basics/fizzBuzz.js
@@ -1,1 +1,14 @@
 // Your code here
+var i;
+
+for (i = 1; i <= 100; i++) {
+  if (i % 3 === 0) {
+    console.log('Fizz');
+  } else {
+    if (i % 5 === 0) {
+      console.log('Buzz');
+    } else {
+      console.log(i);
+    }
+  }
+}
