4ec907b212e280368112299c2b671a93ccdf0b25
Artem Konovalov <artem.konovalov@globallogic.com>
2016-03-31 20:37:22 +0300

    Feat: Pyramid drawing

--- 01-syntax/01-language-basics/pyramid.js
+++ 01-syntax/01-language-basics/pyramid.js
@@ -1,1 +1,28 @@
-// Your code here
+var pyramidLine = '';
+var maxPyramidSize = 15;
+
+function buildPyramid(minLength, maxLength) {
+  var addSpaces = 1;
+  var addPieces = 1;
+  var getReadyValue;
+
+  if (minLength === maxPyramidSize + 2) {
+    getReadyValue = pyramidLine;
+  } else {
+    for (; addSpaces < maxLength / 2; addSpaces++) {
+      pyramidLine += ' ';
+    }
+
+    for (; addPieces <= minLength; addPieces++) {
+      pyramidLine += '^';
+    }
+
+    pyramidLine += '\n';
+
+    getReadyValue = buildPyramid(minLength + 2, maxLength - 2);
+  }
+
+  return getReadyValue;
+}
+
+console.log(buildPyramid(1, maxPyramidSize));
