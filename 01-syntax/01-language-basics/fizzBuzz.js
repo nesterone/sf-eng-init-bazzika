c736a5cee9cd23ef82047741290fef2eeb406494
DeuceX <braga.dmitriy@gmail.com>
2016-03-31 15:16:16 +0300

    Modify: now FizzBuzz appears when devided by 3 and 5

--- 01-syntax/01-language-basics/fizzBuzz.js
+++ 01-syntax/01-language-basics/fizzBuzz.js
@@ -3,6 +3,9 @@ var i;
 
 for (i = 1; i <= 100; i++) {
   if (i % 3 === 0) {
+    if (i % 5 === 0) {
+      console.log('FizzBuzz');
+    }
     console.log('Fizz');
   } else {
     if (i % 5 === 0) {
