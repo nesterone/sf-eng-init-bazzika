aab78cdd8dfc99d25711d693c9799cb3aaedba15
Artem Konovalov <artem.konovalov@globallogic.com>
2016-03-31 19:09:44 +0300

    Feat: First version of chessBoard with fixed area

--- 01-syntax/01-language-basics/chessBoard.js
+++ 01-syntax/01-language-basics/chessBoard.js
@@ -1,1 +1,26 @@
-// Your code here
+var chessLine = '';
+var outerLoop = 1;
+var innerLoop;
+var dividers = [];
+
+function spaceOrSharp(rowIndex) {
+  if (rowIndex % 2 !== 0) {
+    dividers = ['#', ' '];
+  } else {
+    dividers = [' ', '#'];
+  }
+
+  return dividers;
+}
+
+for (; outerLoop <= 8; outerLoop++) {
+  spaceOrSharp(outerLoop);
+
+  for (innerLoop = 1; innerLoop <= 8; innerLoop++) {
+    chessLine += dividers[innerLoop % 2];
+  }
+
+  chessLine += '\n';
+}
+
+console.log(chessLine);
