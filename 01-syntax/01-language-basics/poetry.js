3ae0624fd8352da8f711440926eb2bfe4bb36023
DeuceX <braga.dmitriy@gmail.com>
2016-03-31 15:02:35 +0300

    Implement first varian for poetry

--- 01-syntax/01-language-basics/poetry.js
+++ 01-syntax/01-language-basics/poetry.js
@@ -46,3 +46,36 @@ Take care about tabs, newlines etc.
 */
 
 // Your code here
+console.log('William Blake. 1757–1827');
+
+console.log('The Tiger');
+
+console.log('TIGER, tiger, burning bright');
+console.log('In the forests of the night,');
+console.log('What immortal hand or eye');
+console.log('Could frame thy fearful symmetry?');
+console.log('');
+console.log('In what distant deeps or skies');
+console.log('Burnt the fire of thine eyes?');
+console.log('On what wings dare he aspire?');
+console.log('What the hand dare seize the fire?');
+console.log('');
+console.log('And what shoulder and what art');
+console.log('Could twist the sinews of thy heart?');
+console.log('And when thy heart began to beat,');
+console.log('What dread hand and what dread feet?');
+console.log('');
+console.log('What the hammer? what the chain?');
+console.log('In what furnace was thy brain?');
+console.log('What the anvil? What dread grasp');
+console.log('Dure its deadly terrors clasp?');
+console.log('');
+console.log('When the stars threw down their spears,');
+console.log('And water\'d heaven with their tears,');
+console.log('Did He smile His work to see?');
+console.log('Did He who made the lamb make thee?');
+console.log('');
+console.log('Tiger, tiger, burning bright');
+console.log('In the forests of the night,');
+console.log('What immortal hand or eye');
+console.log('Dare frame thy fearful symmetry?');
