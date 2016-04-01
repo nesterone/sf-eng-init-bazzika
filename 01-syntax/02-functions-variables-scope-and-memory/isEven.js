a948ec570ba1f21a9dac4c8510579d529cff716d
bazzika <daryabelob@gmail.com>
2016-04-01 15:29:30 +0300

    write algorithm to check numbers divisible by two

--- 01-syntax/02-functions-variables-scope-and-memory/isEven.js
+++ 01-syntax/02-functions-variables-scope-and-memory/isEven.js
@@ -1,11 +1,17 @@
 /**
  *  [Recursion](http://eloquentjavascript.net//03_functions.html#p_iDq2OgBOGw)
  *
-*/
+ */
 /* global isEven */
-
-// → Your code here.
-
+function isEven(count) {
+  if (count === 0) {
+    return true;
+  } else if (count === 1) {
+    return false;
+  } else {
+    return (isEven(count - 2));
+  }
+}
 console.log(isEven(50));
 // → true
 console.log(isEven(75));
