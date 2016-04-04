4d059caebc42284c90846665fa56d3e5ea8cf082
Artem Konovalov <artem.konovalov@globallogic.com>
2016-04-04 19:33:20 +0300

    Feat: Add two functions reverseArray and reverseArrayInPlace to file reverseArray.js

--- 01-syntax/03-data-structures/reversing-an-array/reverseArray.js
+++ 01-syntax/03-data-structures/reversing-an-array/reverseArray.js
@@ -6,7 +6,32 @@
 
 var arrayValue = [1, 2, 3, 4, 5];
 
-// → Your code here.
+function reverseArray(inputArray) {
+  var newArray = [];
+  var newArrIndex = 0;
+  var inputArrIndex;
+
+  for (inputArrIndex = inputArray.length - 1; inputArrIndex >= 0; inputArrIndex--) {
+    newArray[newArrIndex] = inputArray[inputArrIndex];
+    newArrIndex += 1;
+  }
+  return newArray;
+}
+
+function reverseArrayInPlace(inputArray) {
+  var leftIndex;
+  var rightIndex = inputArray.length - 1;
+  var arrCenter = Math.floor(inputArray.length / 2);
+  var arr = arguments[0];
+
+  for (leftIndex = 0; leftIndex < arrCenter; leftIndex++) {
+    arr[leftIndex] += arr[rightIndex];
+    arr[rightIndex] = arr[leftIndex] - arr[rightIndex];
+    arr[leftIndex] -= arr[rightIndex];
+    rightIndex -= 1;
+  }
+  return inputArray;
+}
 
 console.log(reverseArray(['A', 'B', 'C']));
 // → ['C', 'B', 'A'];
