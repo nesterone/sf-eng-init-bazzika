39decec9637d27044bb69761e13566feab90c1da
Artem Konovalov <artem.konovalov@globallogic.com>
2016-04-04 17:54:02 +0300

    Feat: Add two functions range and sum to calculate Sum of range(1, 10)

--- 01-syntax/03-data-structures/the-sum-of-ranges/sum.js
+++ 01-syntax/03-data-structures/the-sum-of-ranges/sum.js
@@ -4,7 +4,27 @@
  */
 /* global sum range */
 
-// → Your code here.
+function range(start, end) {
+  var arr = [];
+  var arrIndex = 0;
+  var arrCounter;
+
+  for (arrCounter = start; arrCounter <= end; arrCounter++) {
+    arr[arrIndex] = arrCounter;
+    arrIndex++;
+  }
+  return arr;
+}
+
+function sum(arr) {
+  var arrCount;
+  var sumCalculate = 0;
+
+  for (arrCount = 0; arrCount < arr.length; arrCount++) {
+    sumCalculate += arr[arrCount];
+  }
+  return sumCalculate;
+}
 
 console.log(sum(range(1, 10)));
 // → 55
