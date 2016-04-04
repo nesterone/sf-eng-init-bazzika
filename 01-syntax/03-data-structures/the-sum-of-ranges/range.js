6d9df63996944eb175e0506f0276d401d5755001
bazzika <daryabelob@gmail.com>
2016-04-04 14:13:54 +0300

    - write numbers in order

--- 01-syntax/03-data-structures/the-sum-of-ranges/range.js
+++ 01-syntax/03-data-structures/the-sum-of-ranges/range.js
@@ -3,9 +3,16 @@
  *
  */
 /* global range */
-
-// → Your code here.
-
+function range(start, end) {
+  var i = 0;
+  var row = [];
+  row[0] = start;
+  while (row[i] < end) {
+    row[i + 1] = row[i] + 1;
+    i += 1;
+  }
+  return row;
+}
+console.log(range(1, 10));
 console.log(range(5, 2, -1));
-
 // → [5, 4, 3, 2]
