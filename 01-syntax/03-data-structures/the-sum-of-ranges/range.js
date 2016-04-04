e18a3ee65eb6bbd516e48f60139db869fab57263
bazzika <daryabelob@gmail.com>
2016-04-04 19:16:23 +0300

    - write row of numbers with flexible steps

--- 01-syntax/03-data-structures/the-sum-of-ranges/range.js
+++ 01-syntax/03-data-structures/the-sum-of-ranges/range.js
@@ -3,16 +3,29 @@
  *
  */
 /* global range */
-function range(start, end) {
+function range(start, end, step) {
   var i = 0;
   var row = [];
-  row[0] = start;
-  while (row[i] < end) {
-    row[i + 1] = row[i] + 1;
-    i += 1;
+  var grow = step;
+  i = start;
+  if (typeof grow === 'undefined') {
+    grow = 1;
+  }
+  if (grow >= 0) {
+    while (i <= end) {
+      row.push(i);
+      i += grow;
+    }
+  } else {
+    while (i >= end) {
+      row.push(i);
+      i += step;
+    }
   }
   return row;
 }
 console.log(range(1, 10));
+console.log(range(1, 10, 2));
 console.log(range(5, 2, -1));
 // → [5, 4, 3, 2]
+
