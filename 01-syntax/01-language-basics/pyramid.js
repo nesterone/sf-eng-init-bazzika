41e9054b165ac81701120a08be0c29039ce3e941
DeuceX <braga.dmitriy@gmail.com>
2016-04-04 12:20:42 +0300

    Add implementation with loops

--- 01-syntax/01-language-basics/pyramid.js
+++ 01-syntax/01-language-basics/pyramid.js
@@ -1,12 +1,15 @@
-// Your code here
 var pyramid = '';
+var pyramidLine = '';
+var i;
+var j;
 
-pyramid += '             #\n';
-pyramid += '            ###\n';
-pyramid += '          #######\n';
-pyramid += '        ###########\n';
-pyramid += '       ###############\n';
-pyramid += '     ###################\n';
-pyramid += '   #######################\n';
-pyramid += ' ###########################\n';
-pyramid += '##############################\n';
+for (i = 0; i < 10; i++) {
+  pyramidLine = pyramidLine.trim();
+  pyramidLine = pyramidLine + '##';
+  for (j = 10 - i; j > 0; j--) {
+    pyramidLine = ' ' + pyramidLine;
+  }
+  pyramid += pyramidLine + '\n';
+}
+
+console.log(pyramid);
