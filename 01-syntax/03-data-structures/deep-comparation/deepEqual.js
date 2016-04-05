893f53c4cc0c1956ce40163f5b6ddd105f3f6a7e
Artem Konovalov <artem.konovalov@globallogic.com>
2016-04-05 11:50:50 +0300

    Feat: add function deepEqual and one more additional check, which copmares two objects with different quantity of properties

--- 01-syntax/03-data-structures/deep-comparation/deepEqual.js
+++ 01-syntax/03-data-structures/deep-comparation/deepEqual.js
@@ -4,14 +4,45 @@
  */
 /* global deepEqual */
 
-// → Your code here.
-
 var obj = {
   here: {
     is: 'an'
   },
   object: 2
 };
+
+function deepEqual(firstObj, secondObj) {
+  var theSame = false;
+  var firstKey;
+  var secondKey;
+
+  if (Object.keys(firstObj).length !== Object.keys(secondObj).length) {
+    return false;
+  }
+  for (firstKey in firstObj) {
+    if (firstObj.hasOwnProperty(firstKey)) {
+      for (secondKey in secondObj) {
+        if (secondObj.hasOwnProperty(secondKey) && firstKey === secondKey) {
+          if (firstObj[firstKey] === secondObj[secondKey]) {
+            theSame = true;
+            break;
+          } else if (typeof firstObj[firstKey] === 'object' &&
+            typeof secondObj[secondKey] === 'object' &&
+            firstObj[firstKey] !== null && secondObj[secondKey] !== null) {
+            return deepEqual(firstObj[firstKey], secondObj[secondKey]);
+          }
+        } else {
+          theSame = false;
+        }
+      }
+
+      if (theSame === false) {
+        return false;
+      }
+    }
+  }
+  return true;
+}
 
 console.log(deepEqual(obj, obj));
 // → true
@@ -31,3 +62,13 @@ console.log(
   })
 );
 // → true
+console.log(
+  deepEqual(obj, {
+    here: {
+      is: 'an'
+    },
+    object: 2,
+    testProperty: 5
+  })
+);
+// → false
