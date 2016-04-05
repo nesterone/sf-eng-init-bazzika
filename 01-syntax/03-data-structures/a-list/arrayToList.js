8089314a5f0251f5588cac47c7296a688d62b8bb
Artem Konovalov <artem.konovalov@globallogic.com>
2016-04-05 15:20:45 +0300

    Feat: Add function arrayToList into arrayToList.js

--- 01-syntax/03-data-structures/a-list/arrayToList.js
+++ 01-syntax/03-data-structures/a-list/arrayToList.js
@@ -4,7 +4,23 @@
  */
 /* global arrayToList */
 
-// → Your code here.
+function arrayToList(arr) {
+  var list = {};
+  var arrIndex;
+  var tempReferenceState;
+
+  for (arrIndex = arr.length - 1; arrIndex >= 0; arrIndex--) {
+    tempReferenceState = list;
+    list = {};
+    list.value = arr[arrIndex];
+    if (arrIndex === arr.length - 1) {
+      list.rest = null;
+    } else {
+      list.rest = tempReferenceState;
+    }
+  }
+  return list;
+}
 
 console.log(arrayToList([10, 20]));
 // → {value: 10, rest: {value: 20, rest: null}}
