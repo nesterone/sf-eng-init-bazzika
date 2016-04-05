2a9ed82c2a07ad9100cdfd394e5161b82d5c30f3
DeuceX <braga.dmitriy@gmail.com>
2016-04-05 17:04:14 +0300

    Modify: now you can add a step as an argument

--- 01-syntax/03-data-structures/the-sum-of-ranges/range.js
+++ 01-syntax/03-data-structures/the-sum-of-ranges/range.js
@@ -4,11 +4,24 @@
  */
 /* global range */
 
-function range(start, end) {
+function range(start, end, st) {
   var arr = [];
-  var value = start;
-  for (value; value <= end; value++) {
-    arr.push(value);
+  var begin = start;
+  var fin = end;
+  var step = 1;
+
+  if (!isNaN(parseFloat(st))) {
+    step = st;
+  }
+
+  if (st > 0) {
+    for (begin; begin <= fin; begin += step) {
+      arr.push(begin);
+    }
+  } else {
+    for (begin; begin >= fin; begin += step) {
+      arr.push(begin);
+    }
   }
   return arr;
 }
