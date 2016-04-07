df408ddce685a48d638afb75d72811445b65719b
Artem Konovalov <artem.konovalov@globallogic.com>
2016-04-07 18:37:50 +0300

    Feat: Add function flatten into flatten.js file

--- 01-syntax/04-high-order-functions/flatten/flatten.js
+++ 01-syntax/04-high-order-functions/flatten/flatten.js
@@ -1,7 +1,12 @@
 /* global flatten */
 
-// → Your code here.
-
 var arrays = [[1, 2, 3], [4, 5], [6]];
+
+function flatten(array) {
+  return array.reduce(function (prev, current) {
+    return prev.concat(current);
+  });
+}
+
 console.log(flatten(arrays));
 // → [1, 2, 3, 4, 5, 6]
