85f2a93247311235b5b7b0e10b4026bd155ba1e1
DeuceX <braga.dmitriy@gmail.com>
2016-04-07 17:32:54 +0300

    Add function for deep comparison

--- 01-syntax/03-data-structures/deep-comparation/deepEqual.js
+++ 01-syntax/03-data-structures/deep-comparation/deepEqual.js
@@ -4,9 +4,39 @@
  */
 /* global deepEqual */
 
-// → Your code here.
+var obj;
 
-var obj = {
+function deepEqual(item1, item2) {
+  var key1;
+  var key2;
+  var match;
+  if (typeof item1 === 'object' && typeof item2 === 'object' && item1 && item2) {
+    if (Object.keys(item1).length !== Object.keys(item2).length) return false;
+    for (key1 in item1) {
+      if (item1.hasOwnProperty(key1)) {
+        match = false;
+        for (key2 in item2) {
+          if (key1 === key2) {
+            if (typeof item1[key1] === 'object' && typeof item2[key2] === 'object'
+              && item1[key1] && item2[key2]) {
+              match = deepEqual(item1[key1], item2[key2]);
+            } else {
+              match = (item1[key1] === item2[key2]);
+            }
+          }
+        }
+        if (!match) return false;
+      }
+    }
+    return true;
+  }
+  if (item1 === item2) {
+    return true;
+  }
+  return false;
+}
+
+obj = {
   here: {
     is: 'an'
   },
