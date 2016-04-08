e49022310c0b55172fbeb1d54c16a685836e288f
Artem Konovalov <artem.konovalov@globallogic.com>
2016-04-08 14:30:47 +0300

    Feat: Add two functions Every and Some into the everyAndThenSome.js file

--- 01-syntax/04-high-order-functions/every-and-some/everyAndThenSome.js
+++ 01-syntax/04-high-order-functions/every-and-some/everyAndThenSome.js
@@ -1,6 +1,26 @@
 /* global every some */
 
-// → Your code here.
+function every(arr, func) {
+  var index;
+
+  for (index = 0; index < arr.length; index++) {
+    if (!func(arr[index])) {
+      return false;
+    }
+  }
+  return true;
+}
+
+function some(arr, func) {
+  var index;
+
+  for (index = 0; index < arr.length; index++) {
+    if (func(arr[index])) {
+      return true;
+    }
+  }
+  return false;
+}
 
 console.log(every([NaN, NaN, NaN], isNaN));
 // → true
