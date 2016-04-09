1dc6de0301575da6989a7b5fee913bfabaf71b21
Artem Konovalov <art.gennad.konovalov@gmail.com>
2016-04-09 15:58:53 +0300

    Fix: A long comment with some explanations is removed

--- 01-syntax/02-functions-variables-scope-and-memory/isEven.js
+++ 01-syntax/02-functions-variables-scope-and-memory/isEven.js
@@ -19,27 +19,3 @@ console.log(isEven(75));
 // → false
 console.log(isEven(-1));
 // → Uncaught RangeError: Maximum call stack size exceeded
-
-/* We can to multiply the number to '-1' value.
- This operation inverts a sign of expression
-
- function isEven(number) {
- if (number < 0) {
- number *= -1;
- }
-
- if (number === 0) {
- return true;
- } else if (number === 1) {
- return false;
- }
- return isEven(number - 2);
- }
-
- console.log(isEven(50));
- // → true
- console.log(isEven(75));
- // → false
- console.log(isEven(-1));
- // → false
- */
