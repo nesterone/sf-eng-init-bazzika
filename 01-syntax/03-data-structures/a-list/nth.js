96ce6a037bc705cabdb01354e4c39374651a0ba9
DeuceX <braga.dmitriy@gmail.com>
2016-04-11 18:31:28 +0300

    Add more test samples

--- 01-syntax/03-data-structures/a-list/nth.js
+++ 01-syntax/03-data-structures/a-list/nth.js
@@ -8,6 +8,10 @@ function nth(list, number) {
   var i;
   var deepList = list;
 
+  if (number < 0) {
+    return 'Error! Number should be >= 0';
+  }
+
   for (i = 0; i < number; i++) {
     if (deepList) {
       deepList = deepList.rest;
@@ -21,3 +25,11 @@ function nth(list, number) {
 
 console.log(nth(arrayToList([10, 20, 30]), 1));
 // → 20
+console.log(nth(arrayToList([10, 20, 30]), 0));
+// → 10
+console.log(nth(arrayToList([10, 20, 30]), 2));
+// → 30
+console.log(nth(arrayToList([10, 20, 30]), 6));
+// → error
+console.log(nth(arrayToList([10, 20, 30]), -1));
+// → error
