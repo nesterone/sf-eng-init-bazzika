31acd587e3cb3452df74a1939410eb65344e6353
DeuceX <braga.dmitriy@gmail.com>
2016-04-11 18:40:05 +0300

    Add isObject function and add more test cases

--- 01-syntax/03-data-structures/deep-comparation/deepEqual.js
+++ 01-syntax/03-data-structures/deep-comparation/deepEqual.js
@@ -5,19 +5,25 @@
 /* global deepEqual */
 
 var obj;
+var obj2;
+
+function isObject(testObject) {
+  if (typeof testObject === 'object') return true;
+  return false;
+}
 
 function deepEqual(item1, item2) {
   var key1;
   var key2;
   var match;
-  if (typeof item1 === 'object' && typeof item2 === 'object' && item1 && item2) {
+  if (isObject(item1) && isObject(item2) && item1 && item2) {
     if (Object.keys(item1).length !== Object.keys(item2).length) return false;
     for (key1 in item1) {
       if (item1.hasOwnProperty(key1)) {
         match = false;
         for (key2 in item2) {
           if (key1 === key2) {
-            if (typeof item1[key1] === 'object' && typeof item2[key2] === 'object'
+            if (isObject(item1[key1]) && isObject(item2[key2])
               && item1[key1] && item2[key2]) {
               match = deepEqual(item1[key1], item2[key2]);
             } else {
@@ -42,6 +48,14 @@ obj = {
   },
   object: 2
 };
+obj2 = {
+  one: 5,
+  two: {
+    three: {
+      four: 4
+    }
+  }
+};
 
 console.log(deepEqual(obj, obj));
 // → true
@@ -61,3 +75,25 @@ console.log(
   })
 );
 // → true
+console.log(deepEqual(obj2, obj2));
+// → true
+console.log(deepEqual(obj2, {
+  one: 5,
+  two: {
+    three: {
+      four: 4
+    }
+  }
+}));
+// → true
+console.log(deepEqual(obj2, {
+  one: 5,
+  two: {
+    three: {
+      four: {
+        five: 5
+      }
+    }
+  }
+}));
+// → false
