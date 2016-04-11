9c8b3d7608f5b011ed972b77a7ac895371afcc26
Artem Konovalov <artem.konovalov@globallogic.com>
2016-04-11 19:48:43 +0300

    Feat: Add few checks to both files countBs.js and countChar.js

--- 01-syntax/02-functions-variables-scope-and-memory/bean-counting/countBs.js
+++ 01-syntax/02-functions-variables-scope-and-memory/bean-counting/countBs.js
@@ -8,8 +8,13 @@ function countBs(text) {
   var charCount;
   var numberOfCharacters = 0;
 
+  if (! text || text.length === 0 ||
+    (typeof text !== 'string' && text.constructor !== Array)) {
+    return 0;
+  }
+
   for (charCount = 0; charCount < text.length; charCount++) {
-    if (text.charAt(charCount) === 'B') {
+    if (text[charCount] === 'B') {
       numberOfCharacters += 1;
     }
   }
@@ -19,3 +24,28 @@ function countBs(text) {
 
 console.log(countBs('BBC'));
 // → 2
+console.log(countBs(['a', 'B', '0', 'B']));
+// → 2
+
+console.log(countBs({}));
+// → 0
+console.log(countBs(0));
+// → 0
+console.log(countBs(''));
+// → 0
+console.log(countBs('2'));
+// → 0
+console.log(countBs([]));
+// → 0
+console.log(countBs(null));
+// → 0
+console.log(countBs(undefined));
+// → 0
+console.log(countBs(NaN));
+// → 0
+console.log(countBs(Infinity));
+// → 0
+console.log(countBs(-Infinity));
+// → 0
+console.log(countBs());
+// → 0
--- 01-syntax/02-functions-variables-scope-and-memory/bean-counting/countChar.js
+++ 01-syntax/02-functions-variables-scope-and-memory/bean-counting/countChar.js
@@ -8,8 +8,13 @@ function countChar(text, character) {
   var charCount;
   var numberOfCharacters = 0;
 
+  if (! text || text.length === 0 ||
+    (typeof text !== 'string' && text.constructor !== Array)) {
+    return 0;
+  }
+
   for (charCount = 0; charCount < text.length; charCount++) {
-    if (text.charAt(charCount) === character) {
+    if (text[charCount] === character) {
       numberOfCharacters += 1;
     }
   }
@@ -19,3 +24,27 @@ function countChar(text, character) {
 
 console.log(countChar('kakkerlak', 'k'));
 // → 4
+console.log(countChar(['a', 'B', '0', 'B'], 'B'));
+// → 2
+console.log(countChar({}, 'B'));
+// → 0
+console.log(countChar(0, 'B'));
+// → 0
+console.log(countChar('', 'B'));
+// → 0
+console.log(countChar('2', 'B'));
+// → 0
+console.log(countChar([], 'B'));
+// → 0
+console.log(countChar(null, 'B'));
+// → 0
+console.log(countChar(undefined, 'B'));
+// → 0
+console.log(countChar(NaN, 'B'));
+// → 0
+console.log(countChar(Infinity, 'B'));
+// → 0
+console.log(countChar(-Infinity, 'B'));
+// → 0
+console.log(countChar('B'));
+// → 0
