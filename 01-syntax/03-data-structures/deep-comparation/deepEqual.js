e29bb3d829e7705215f334fdf25fab6701d3b57e
Artem Konovalov <artem.konovalov@globallogic.com>
2016-04-12 18:59:43 +0300

    Feat: Add a few checks for sunction and change structure of source code

--- 01-syntax/03-data-structures/deep-comparation/deepEqual.js
+++ 01-syntax/03-data-structures/deep-comparation/deepEqual.js
@@ -12,31 +12,34 @@ var obj = {
 };
 
 function deepEqual(firstObj, secondObj) {
-  var theSame = false;
   var firstKey;
   var secondKey;
+  var same;
+
+  arguments[0] = firstObj || {};
+  arguments[1] = secondObj || {};
 
   if (Object.keys(firstObj).length !== Object.keys(secondObj).length) {
     return false;
+  } else if (firstObj === secondObj) {
+    return true;
   }
+
   for (firstKey in firstObj) {
     if (firstObj.hasOwnProperty(firstKey)) {
+      same = false;
       for (secondKey in secondObj) {
-        if (secondObj.hasOwnProperty(secondKey) && firstKey === secondKey) {
-          if (firstObj[firstKey] === secondObj[secondKey]) {
-            theSame = true;
-            break;
-          } else if (typeof firstObj[firstKey] === 'object' &&
-            typeof secondObj[secondKey] === 'object' &&
-            firstObj[firstKey] !== null && secondObj[secondKey] !== null) {
-            return deepEqual(firstObj[firstKey], secondObj[secondKey]);
-          }
-        } else {
-          theSame = false;
+        if (secondObj.hasOwnProperty(secondKey) &&
+          typeof firstObj[firstKey] === 'object' &&
+          typeof secondObj[secondKey] === 'object' &&
+          firstKey === secondKey &&
+          firstObj[firstKey] && secondObj[secondKey]) {
+          same = deepEqual(firstObj[firstKey], secondObj[secondKey]);
+        } else if (firstKey === secondKey) {
+          same = (firstObj[firstKey] === secondObj[secondKey]);
         }
       }
-
-      if (theSame === false) {
+      if (!same) {
         return false;
       }
     }
@@ -53,6 +56,7 @@ console.log(
   })
 );
 // → false
+
 console.log(
   deepEqual(obj, {
     here: {
@@ -67,8 +71,47 @@ console.log(
     here: {
       is: 'an'
     },
+    object: 5
+  })
+);
+// → false
+console.log(
+  deepEqual({
+    here: {
+      is: 'an',
+      obj: 5
+    },
+    object: 2
+  }, {
+    here: {
+      is: 'an',
+      obj: 5
+    },
+    object: 2
+  })
+);
+// → true
+console.log(
+  deepEqual(obj, {
+    here: {
+      is: 'an'
+    },
     object: 2,
     testProperty: 5
   })
 );
+// → false
+console.log(
+  deepEqual(obj, {
+    object: 10,
+    here: {
+      is: 'an'
+    }
+  }));
+// → false
+console.log(deepEqual(obj, null));
+// → false
+console.log(deepEqual(null, null));
+// → true
+console.log(deepEqual(undefined, obj));
 // → false
