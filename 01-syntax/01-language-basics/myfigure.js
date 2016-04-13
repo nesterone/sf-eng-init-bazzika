c080088da3e84cc38e34b96cd28082ecd8151154
Artem Konovalov <artem.konovalov@globallogic.com>
2016-04-13 10:34:36 +0300

    Feat: Change my figure. Now it paints the Money Tree

--- 01-syntax/01-language-basics/myfigure.js
+++ 01-syntax/01-language-basics/myfigure.js
@@ -1,22 +1,32 @@
-console.log('\t\t\t\t*\t\t\t\t\t\t\t\t\t\t\t*\t\t\t\t\t\t\t\t*');
-console.log('\t\t\t\t\t\t\t*\t\t\t\t\t\t\t\t\t\t\t*');
-console.log('\t*\t\t\t\t\t\t\t\t\t*\t\t\t\t*\t\t\t\t\t\t*\t\t\t\t\t*');
-console.log('\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t __(_)__');
-console.log('\t\t\t\t*\t\t\t\t\t\t\t\t*\t\t\t\t\t\t\t\t\t\t(_*___*_)');
-console.log('\t\t*\t  ===  =====   ===   ====\t\t==\t\t*\t   ==  ===\t ====\t ===\t\t  |\t  |');
-console.log('*\t\t\t =\t\t =\t  =\t  =\t =\t =\t\t  =\t\t\t  =\t  =\t  =\t =\t =\t=');
-console.log('\t\t\t  ===\t =\t  = = =\t ====\t *\t   =\t=\t =\t  = = =\t ====\t ===');
-console.log('\t  *\t\t\t =\t =\t  =\t  =\t =\t =\t\t\t=  = =\t=  *  =\t  =  =\t =\t\t=\t*');
-console.log('\t*\t\t  ===\t =\t  = ` =\t =\t  =\t\t\t ==\t  ==\t  =\t  =\t =\t  =\t ===');
-console.log('\t\t\t\t\t\t\t`\t*\t\t\t\t\t\t\t\t  *\t\t\t\t\t*\t\t\t\t\t\t\t*');
-console.log('\t\t\t*\t\t\t\t`\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t*');
-console.log('\t\t\t\t\t\t\t`');
-console.log('\t\t*\t\t\t\t\t\t\t\t\t*\t\t\t\t\t\t\t\t\t\t\t\t\t\t*');
-console.log('\t\t\t\t\t\t\t|\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t `');
-console.log('\t*\t\t\t\t   *   [_]\t\t\t\t\t\t\t\t*\t\t\t\t\t\t  *\t `\t\t\t\t\t\t\t\t\t*');
-console.log('\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t `');
-console.log('\t\t\t*\t\t\t\t\t\t*\t\t\t\t\t\t\t\t\t*\t\t\t\t\t\t\t\t\t\t*');
-console.log('\t  *\t\t\t\t*');
-console.log('\t\t\t\t\t\t\t\t\t\t  __( )__\t\t\t\t\t\t\t\t\t\t |');
-console.log('\t\t\t\t*\t\t\t\t\t\t (_*___*_)\t\t\t\t*\t\t\t\t\t\t[_]\t\t\t\t\t\t\t\t*');
-console.log('\t\t\t\t\t\t\t\t\t\t   |   |');
+var moneyTreeSize = 20;
+var moneyTree = '';
+var addNeedle = 1;
+var branchLength;
+var spaces;
+var rows;
+
+for (rows = 0; rows < (moneyTreeSize / 2); rows++) {
+  for (spaces = 0; spaces < moneyTreeSize / 2 - rows; spaces++) {
+    moneyTree += ' ';
+  }
+
+  for (branchLength = 0; branchLength < addNeedle; branchLength++) {
+    moneyTree += '$';
+  }
+
+  moneyTree += '\n';
+
+  for (spaces = 0; spaces < moneyTreeSize / 2; spaces++) {
+    moneyTree += ' ';
+  }
+
+  moneyTree += '#\n';
+  addNeedle += 2;
+}
+
+while (moneyTreeSize + 2) {
+  moneyTree += '+';
+  moneyTreeSize -= 1;
+}
+
+console.log(moneyTree);
