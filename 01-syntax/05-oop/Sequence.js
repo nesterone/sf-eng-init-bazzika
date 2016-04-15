c10cf26316afeebc07d4c4b6f46510ad223223d6
Artem Konovalov <artem.konovalov@globallogic.com>
2016-04-15 18:52:26 +0300

    Add interface and his realization

--- 01-syntax/05-oop/Sequence.js
+++ 01-syntax/05-oop/Sequence.js
@@ -1,1 +1,52 @@
-// → Your code here.
+function Interface(arr) {
+  this.arr = arr;
+}
+
+Interface.prototype.fillObjArray = function (start, end) {
+  while (start <= end) {
+    this.arr.push(start);
+    arguments[0]++;
+  }
+};
+
+Interface.prototype.start = function () {
+  this.indexPosition = 0;
+  return this.indexPosition;
+};
+
+Interface.prototype.current = function () {
+  return this.indexPosition++;
+};
+
+Interface.prototype.finish = function () {
+  return this.arr.length;
+};
+
+Interface.prototype.iterator = function (curIndex) {
+  if (curIndex < this.arr.length) {
+    return this.arr[curIndex];
+  }
+  return undefined;
+};
+
+function ArraySeq(arr) {
+  return new Interface(arr);
+}
+
+function RangeSeq(start, end) {
+  var obj = new Interface([]);
+  obj.fillObjArray(start, end);
+  return obj;
+}
+
+function logFive(obj) {
+  var index;
+
+  for (index = obj.start(); index < obj.finish() && index < 5; index++) {
+    console.log(obj.iterator(obj.current()));
+  }
+}
+
+logFive(new ArraySeq([1, 2]));
+
+logFive(new RangeSeq(100, 1000));
