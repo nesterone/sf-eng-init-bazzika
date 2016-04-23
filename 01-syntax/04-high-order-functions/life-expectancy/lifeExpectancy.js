aef8ae5f98eb3356debb9868c869b6d4f7a80019
bazzika <daryabelob@gmail.com>
2016-04-23 17:00:51 +0300

    able to calculate average age,cesond variant of code

--- 01-syntax/04-high-order-functions/life-expectancy/lifeExpectancy.js
+++ 01-syntax/04-high-order-functions/life-expectancy/lifeExpectancy.js
@@ -1,44 +1,41 @@
 /* global ancestry lifeExpectancy */
-function average(array) {
-  function plus(a, b) {
-    return a + b;
-  }
-
-  return array.reduce(plus) / array.length;
-}
-console.log(average([2, 2, 2]));
-// → 2
 function lifeExpectancy(ancestry) {
-  var ages;
-  var curcentury;
+  var currentCentury;
   var currentAge;
   function groupBy(array, viaCentury) {
     var century = {};
     array.forEach(function (person) {
-      var keycentury;
-      keycentury = viaCentury(person);
-      if (keycentury in century) {
-        century[keycentury].push(person);
+      var keyCentury;
+      keyCentury = viaCentury(person);
+      if (keyCentury in century) {
+        century[keyCentury].push(person);
       } else {
-        century[keycentury] = [];
-        century[keycentury].push(person);
+        century[keyCentury] = [];
+        century[keyCentury].push(person);
       }
     });
     return century;
   }
 
-  curcentury = groupBy(ancestry, function (person) {
+  currentCentury = groupBy(ancestry, function (person) {
     return Math.ceil(person.died / 100);
   });
-  for (currentAge in curcentury) {
-    if (curcentury.hasOwnProperty(currentAge)) {
-      ages = curcentury[currentAge].map(function (person) {
+  for (currentAge in currentCentury) {
+    if (currentCentury.hasOwnProperty(currentAge)) {
+      currentCentury[currentAge] = currentCentury[currentAge].map(function (person) {
         return person.died - person.born;
+      }).reduce(function average(a, b, index, array) {
+        var result;
+        if (index < array.length - 1) {
+          result = a + b;
+        } else {
+          result = Math.round(((a + b) / array.length) * 10) / 10;
+        }
+        return result;
       });
-      curcentury[currentAge] = Math.round(average(ages) * 10) / 10;
     }
   }
-  return curcentury;
+  return currentCentury;
 }
 console.log(lifeExpectancy(ancestry));
 // → 16: 43.5
