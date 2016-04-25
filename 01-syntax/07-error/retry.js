e3ceb0ba33ccace68c9f07de7fab78e01c762240
DeuceX <braga.dmitriy@gmail.com>
2016-04-25 15:36:20 +0300

    Add retry implementation

--- 01-syntax/07-error/retry.js
+++ 01-syntax/07-error/retry.js
@@ -1,1 +1,21 @@
-// Your code here.
+function MultiplicatorUnitFailure() {}
+
+function primitiveMultiply(a, b) {
+  if (Math.random() < 0.5) return a * b;
+  throw new MultiplicatorUnitFailure();
+}
+
+function reliableMultiply(a, b) {
+  var result;
+  for (;;) {
+    try {
+      result = primitiveMultiply(a, b);
+      return result;
+    } catch (err) {
+      console.log('Failed. Trying to multiply again...');
+    }
+  }
+}
+
+console.log(reliableMultiply(8, 8));
+// → 64
