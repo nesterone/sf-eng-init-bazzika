0f810721ba736cc6b344cc6f5701ecb0caf494ce
Artem Konovalov <artem.konovalov@globallogic.com>
2016-04-25 14:33:30 +0300

    Hide global functions into ageDifference function

--- 01-syntax/04-high-order-functions/mother-child-age/ageDifference.js
+++ 01-syntax/04-high-order-functions/mother-child-age/ageDifference.js
@@ -1,24 +1,20 @@
-/* global ancestry ageDifference*/
-
-var byName = {};
-
-function average(array) {
-  function plus(a, b) {
-    return a + b;
-  }
-
-  return array.reduce(plus) / array.length;
-}
-
-console.log(average([2, 2, 2]));
-// → 2
-
-ancestry.forEach(function (person) {
-  byName[person.name] = person;
-});
+/* global ancestry */
 
 function ageDifference(incomingAncestry) {
   var diffAge = [];
+  var byName = {};
+
+  function average(array) {
+    function plus(a, b) {
+      return a + b;
+    }
+
+    return array.reduce(plus) / array.length;
+  }
+
+  ancestry.forEach(function (person) {
+    byName[person.name] = person;
+  });
 
   incomingAncestry.reduce(function (sum, current) {
     if (byName[current.mother]) {
