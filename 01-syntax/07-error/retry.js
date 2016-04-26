e94f65d3c35a9c3733f3295701e95cb7de7bec2c
Artem Konovalov <artem.konovalov@globallogic.com>
2016-04-26 17:19:35 +0300

    Feat: Add retry exercise solution

--- 01-syntax/07-error/retry.js
+++ 01-syntax/07-error/retry.js
@@ -1,1 +1,24 @@
-// Your code here.
+function MultiplicatorUnitFailure() {}
+
+function primitiveMultiply(a, b) {
+  if (Math.random() < 0.5) {
+    return a * b;
+  }
+  throw new MultiplicatorUnitFailure();
+}
+
+function reliableMultiply(a, b) {
+  for (;;) {
+    try {
+      return primitiveMultiply(a, b);
+    } catch (error) {
+      if (!(error instanceof MultiplicatorUnitFailure)) {
+        throw new MultiplicatorUnitFailure();
+      }
+    }
+  }
+}
+
+console.log(reliableMultiply(8, 8));
+// → 64
+
