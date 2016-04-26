d10f6a293a493c9b3815cd5e638522313cc106a3
Artem Konovalov <artem.konovalov@globallogic.com>
2016-04-26 19:02:55 +0300

    Feat Add The Locked Box Solution

--- 01-syntax/07-error/lockedBox.js
+++ 01-syntax/07-error/lockedBox.js
@@ -1,1 +1,44 @@
-// Your code here.
+var box = {
+  locked: true,
+  unlock: function () {
+    this.locked = false;
+  },
+  lock: function () {
+    this.locked = true;
+  },
+  _content: [],
+  get content() {
+    if (this.locked) {
+      throw new Error('Locked!');
+    }
+    return this._content;
+  }
+};
+
+function withBoxUnlocked(body) {
+  if (box.locked) {
+    box.unlock();
+  }
+
+  try {
+    body();
+  } catch (error) {
+    console.log(error.message);
+  } finally {
+    box.lock();
+  }
+}
+
+withBoxUnlocked(function () {
+  box.content.push('gold piece');
+});
+
+try {
+  withBoxUnlocked(function () {
+    throw new Error('Pirates on the horizon! Abort!');
+  });
+} catch (e) {
+  throw new Error('Error raised:', e);
+}
+console.log(box.locked);
+// → true.
