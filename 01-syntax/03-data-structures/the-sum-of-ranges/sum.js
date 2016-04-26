734ba865ce88aefd0165b53e43f2bf248bf9158c
bazzika <daryabelob@gmail.com>
2016-04-26 13:45:16 +0300

    bring up reference to range in code

--- 01-syntax/03-data-structures/the-sum-of-ranges/sum.js
+++ 01-syntax/03-data-structures/the-sum-of-ranges/sum.js
@@ -3,32 +3,11 @@
  *
  */
 /* global sum range */
-function range(start, end, step) {
-  var i = 0;
-  var row = [];
-  var grow = step;
-  i = start;
-  if (typeof grow === 'undefined') {
-    grow = 1;
-  }
-  if (grow >= 0) {
-    while (i <= end) {
-      row.push(i);
-      i += grow;
-    }
-  } else {
-    while (i >= end) {
-      row.push(i);
-      i += step;
-    }
-  }
-  return row;
-}
 function sum(arr) {
   var result = 0;
   var j;
-  for (j = 0, j < arr.length, j++) {
-    result += arr[i];
+  for (j = 0; j < arr.length; j++) {
+    result += arr[j];
   }
   return result;
 }
