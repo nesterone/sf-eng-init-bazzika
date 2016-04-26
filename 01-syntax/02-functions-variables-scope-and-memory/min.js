6e39abbf94da0af6562148a5d9d6b2bd1875f92d
Artem Konovalov <artem.konovalov@globallogic.com>
2016-04-26 10:22:29 +0300

    Add more test cases to coursse content

--- 01-syntax/02-functions-variables-scope-and-memory/min.js
+++ 01-syntax/02-functions-variables-scope-and-memory/min.js
@@ -10,5 +10,23 @@ console.log(min(0, 10));
 // → 0
 console.log(min(0, -10));
 // → -10
-console.log(min(0, -10));
-// → -10
+console.log(min(10, 10));
+// → 10
+console.log(min(-10, -20));
+// → -20
+console.log(min('10', '15'));
+// → '10'
+console.log(min('100', '15'));
+// → '15'
+console.log(min(10));
+// → undefined
+console.log(min());
+// → undefined
+console.log(min(-Infinity, Infinity));
+// → -Infinity
+console.log(min(0, Infinity));
+// → 0
+console.log(min(0, NaN));
+// → undefined
+console.log(min(0, null));
+// → undefined
