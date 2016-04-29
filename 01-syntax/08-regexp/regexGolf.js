fb5aec23ec128ad6e54749ecfd6db61a526b0ea9
Artem Konovalov <artem.konovalov@globallogic.com>
2016-04-29 11:18:05 +0300

    Feat: Add solution for RegexpGolf Exercise

--- 01-syntax/08-regexp/regexGolf.js
+++ 01-syntax/08-regexp/regexGolf.js
@@ -1,1 +1,44 @@
-// Your code here.
+function verify(regexp, yes, no) {
+  if (regexp.source === '...') {
+    return;
+  }
+
+  yes.forEach(function (s) {
+    if (!regexp.test(s)) {
+      console.log("Failure to match '" + s + "'");
+    }
+  });
+  no.forEach(function (s) {
+    if (regexp.test(s)) {
+      console.log("Unexpected match for '" + s + "'");
+    }
+  });
+}
+
+verify(/ca[rt]/,
+  ['my car', 'bad cats'],
+	['camper', 'high art']);
+
+verify(/pr?op/,
+	['pop culture', 'mad props'],
+	['plop']);
+
+verify(/ferr(et|y|ari)/,
+	['ferret', 'ferry', 'ferrari'],
+	['ferrum', 'transfer A']);
+
+verify(/.*ious\b/,
+	['how delicious', 'spacious room'],
+	['ruinous', 'consciousness']);
+
+verify(/\s[.,:;]/,
+	['bad punctuation .'],
+	['escape the dot']);
+
+verify(/\w{7,}/,
+	['hottentottententen'],
+	['no', 'hotten totten tenten']);
+
+verify(/\b[^e\s]+\b/,
+	['red platypus', 'wobbling nest'],
+	['earth bed', 'learning ape']);
