164936b22a25b1126046515b645029d6d1e6a454
Artem Konovalov <artem.konovalov@globallogic.com>
2016-05-17 18:59:16 +0300

    Feat: Add solution for Month Names Exercise

--- 01-syntax/09-modules/month-names/monthNames.js
+++ 01-syntax/09-modules/month-names/monthNames.js
@@ -1,3 +1,20 @@
 /**
  * Created by nesterone on 11/1/15.
  */
+
+var month = (function () {
+  var names = ['January', 'February', 'March', 'April', 'May', 'June',
+	'July', 'August', 'September', 'October', 'November', 'December'];
+
+  if (!month) {
+    return {
+      name: function (number) {
+        return names[number];
+      },
+      number: function (name) {
+        return names.indexOf(name);
+      }
+    };
+  }
+  return undefined;
+}());
