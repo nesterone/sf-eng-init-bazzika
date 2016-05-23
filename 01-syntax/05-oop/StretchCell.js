9e1ee243a2d8d9e736abba6a0a21d392bc16bdc7
DeuceX <braga.dmitriy@gmail.com>
2016-05-23 15:23:01 +0300

    Remove unused variables

--- 01-syntax/05-oop/StretchCell.js
+++ 01-syntax/05-oop/StretchCell.js
@@ -1,52 +1,4 @@
-var MOUNTAINS = [
-  { name: 'Kilimanjaro', height: 5895, country: 'Tanzania' },
-  { name: 'Everest', height: 8848, country: 'Nepal' },
-  { name: 'Mount Fuji', height: 3776, country: 'Japan' },
-  { name: 'Mont Blanc', height: 4808, country: 'Italy/France' },
-  { name: 'Vaalserberg', height: 323, country: 'Netherlands' },
-  { name: 'Denali', height: 6168, country: 'United States' },
-  { name: 'Popocatepetl', height: 5465, country: 'Mexico' }
-];
-
 var sc;
-
-function rowHeights(rows) {
-  return rows.map(function (row) {
-    return row.reduce(function (max, cell) {
-      return Math.max(max, cell.minHeight());
-    }, 0);
-  });
-}
-
-function colWidths(rows) {
-  return rows[0].map(function (_, i) {
-    return rows.reduce(function (max, row) {
-      return Math.max(max, row[i].minWidth());
-    }, 0);
-  });
-}
-
-function drawTable(rows) {
-  var heights = rowHeights(rows);
-  var widths = colWidths(rows);
-
-  function drawLine(blocks, lineNo) {
-    return blocks.map(function (block) {
-      return block[lineNo];
-    }).join(' ');
-  }
-
-  function drawRow(row, rowNum) {
-    var blocks = row.map(function (cell, colNum) {
-      return cell.draw(widths[colNum], heights[rowNum]);
-    });
-    return blocks[0].map(function (_, lineNo) {
-      return drawLine(blocks, lineNo);
-    }).join('\n');
-  }
-
-  return rows.map(drawRow).join('\n');
-}
 
 function repeat(string, times) {
   var result = '';
@@ -93,23 +45,9 @@ UnderlinedCell.prototype.draw = function (width, height) {
     .concat([repeat('-', width)]);
 };
 
-function dataTable(data) {
-  var keys = Object.keys(data[0]);
-  var headers = keys.map(function (name) {
-    return new UnderlinedCell(new TextCell(name));
-  });
-  var body = data.map(function (row) {
-    return keys.map(function (name) {
-      return new TextCell(String(row[name]));
-    });
-  });
-  return [headers].concat(body);
-}
-
-console.log(drawTable(dataTable(MOUNTAINS)));
 
 function StretchCell(inner, width, height) {
-  var text = this.inner.text;
+  var text = inner.text;
   var cellHeight = height;
   this.inner = inner;
   if (text.length < cellHeight) {
