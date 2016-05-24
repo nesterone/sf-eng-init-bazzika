48de89ad2aa387d38c134ddf558c73f4c5b2b3b5
Artem Konovalov <artem.konovalov@globallogic.com>
2016-05-24 16:32:24 +0300

    Feat: Add array support to the Egg project

--- 01-syntax/10-project-egg/egg.js
+++ 01-syntax/10-project-egg/egg.js
@@ -178,6 +178,22 @@ topEnv.print = function (value) {
   return value;
 };
 
+topEnv.array = function () {
+  if (!arguments) {
+    throw new SyntaxError('Too few arguments for an array');
+  }
+
+  return Array.prototype.slice.call(arguments);
+};
+
+topEnv.length = function (array) {
+  return array.length;
+};
+
+topEnv.element = function (array, index) {
+  return array[index];
+};
+
 function run() {
   var env = Object.create(topEnv);
   var program = Array.prototype.slice.call(arguments, 0).join('\n');
@@ -221,6 +237,13 @@ specialForms.fun = function (args, env) {
   };
 };
 
-if (run) {
-  console.log('run is loaded!');
-}
+
+run('do(define(sum, fun(array,',
+  '     do(define(i, 0),',
+  '        define(sum, 0),',
+  '        while(<(i, length(array)),',
+  '          do(define(sum, +(sum, element(array, i))),',
+  '             define(i, +(i, 1)))),',
+  '        sum))),',
+  '   print(sum(array(1, 2, 3))))');
+// → 6
