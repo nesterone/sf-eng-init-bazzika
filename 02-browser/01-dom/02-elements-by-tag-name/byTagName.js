4b742d3cf00199d8922091124bc2f0c88545b82a
bazzika <daryabelob@gmail.com>
2016-05-27 16:54:29 +0300

    write function which Implement version of the getElementByTagName method

--- 02-browser/01-dom/02-elements-by-tag-name/byTagName.js
+++ 02-browser/01-dom/02-elements-by-tag-name/byTagName.js
@@ -1,9 +1,22 @@
 var paragraph = document.querySelector('p');
+function byTagName(allNodes, string) {
+  var array = [];
+  var tagName = string.toUpperCase();
+  function inner(node) {
+    var i;
+    if (node.nodeType === document.ELEMENT_NODE) {
+      for (i = 0; i <= node.childNodes.length - 1; i++) {
+        if (node.childNodes[i].tagName === tagName) {
+          array.push(node.childNodes[i].tagName);
+        }
+        inner(node.childNodes[i]);
+      }
+    }
+    return array;
+  }
 
-function byTagName() {
-  // Your code here.
+  return inner(allNodes);
 }
-
 console.log(byTagName(document.body, 'h1').length);
 // → 1
 console.log(byTagName(document.body, 'span').length);
