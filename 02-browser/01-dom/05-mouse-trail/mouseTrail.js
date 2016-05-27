bc81edb5fba77886089072dd91483d047b784a4d
DeuceX <braga.dmitriy@gmail.com>
2016-05-27 15:19:55 +0300

    Add mouse trail implementation

--- 02-browser/01-dom/05-mouse-trail/mouseTrail.js
+++ 02-browser/01-dom/05-mouse-trail/mouseTrail.js
@@ -1,1 +1,15 @@
-// Your code here.
+addEventListener('mousemove', function (event) {
+  var trails;
+  var tail = document.createElement('div');
+  tail.className = 'trail';
+  tail.style.top = event.pageY + 'px';
+  tail.style.left = event.pageX + 'px';
+  document.body.appendChild(tail);
+  setTimeout(function () {
+    document.body.removeChild(tail);
+  }, 2000);
+  trails = document.querySelectorAll('.trail');
+  if (trails.length > 150) {
+    trails[0].parentNode.removeChild(trails[0]);
+  }
+});
