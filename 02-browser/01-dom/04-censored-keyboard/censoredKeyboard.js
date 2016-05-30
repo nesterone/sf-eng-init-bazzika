abf63026ce24d67bb5bdba956c1dfd1d9448957d
bazzika <daryabelob@gmail.com>
2016-05-30 17:26:02 +0300

    commit out Eslint

--- 02-browser/01-dom/04-censored-keyboard/censoredKeyboard.js
+++ 02-browser/01-dom/04-censored-keyboard/censoredKeyboard.js
@@ -1,1 +1,10 @@
-// Your code here.
+var field = document.querySelector('input');
+
+field.addEventListener('keydown', function (event) {
+  if (event.keyCode === 'Q'.charCodeAt(0) ||
+    event.keyCode === 'W'.charCodeAt(0) ||
+    event.keyCode === 'X'.charCodeAt(0)) {
+    event.preventDefault();
+  }
+});
+/* Q,W,X cannot be typed into text file */
