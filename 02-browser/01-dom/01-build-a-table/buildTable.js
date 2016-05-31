95e1e88a939a66fb5b050c22988260da82b9935a
Artem Konovalov <artem.konovalov@globallogic.com>
2016-05-31 11:27:45 +0300

    Feat: Add solution for BuildATable exercise

--- 02-browser/01-dom/01-build-a-table/buildTable.js
+++ 02-browser/01-dom/01-build-a-table/buildTable.js
@@ -1,7 +1,42 @@
-var MOUNTAINS;
+function buildTable(array) {
+  var table = document.createElement('table');
+  var tr = document.createElement('tr');
+  var th;
+  var td;
+  var key;
 
-function buildTable() {
-  // Your code here.
+  Object.keys(array[0]).forEach(function (value) {
+    th = document.createElement('th');
+    th.appendChild(document.createTextNode(value));
+    tr.appendChild(th);
+    table.appendChild(tr);
+  });
+
+  array.forEach(function (value) {
+    tr = document.createElement('tr');
+
+    for (key in value) {
+      if (value.hasOwnProperty(key)) {
+        td = document.createElement('td');
+        td.appendChild(document.createTextNode(value[key]));
+        tr.appendChild(td);
+        if (typeof value[key] === 'number') {
+          td.style.textAlign = 'right';
+        }
+      }
+    }
+    table.appendChild(tr);
+  });
+
+  return table;
 }
 
-document.body.appendChild(buildTable(MOUNTAINS));
+document.body.appendChild(buildTable([
+	{ name: 'Kilimanjaro', height: 5895, country: 'Tanzania' },
+	{ name: 'Everest', height: 8848, country: 'Nepal' },
+	{ name: 'Mount Fuji', height: 3776, country: 'Japan' },
+	{ name: 'Mont Blanc', height: 4808, country: 'Italy/France' },
+	{ name: 'Vaalserberg', height: 323, country: 'Netherlands' },
+	{ name: 'Denali', height: 6168, country: 'United States' },
+	{ name: 'Popocatepetl', height: 5465, country: 'Mexico' }
+]));
