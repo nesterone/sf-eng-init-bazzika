f9d25125154d51e0f53834c23f5a5d4cff0a95b4
Artem Konovalov <artem.konovalov@globallogic.com>
2016-05-31 14:58:47 +0300

    Feat: Add solution for MouseTrail Exercise

--- 02-browser/01-dom/05-mouse-trail/mouseTrail.js
+++ 02-browser/01-dom/05-mouse-trail/mouseTrail.js
@@ -1,1 +1,13 @@
-// Your code here.
+document.addEventListener('mousemove', function (event) {
+  'use strict';
+
+  var trail = document.createElement('div');
+  trail.className = 'trail';
+  trail.style.top = event.pageY + 'px';
+  trail.style.left = event.pageX + 'px';
+  document.body.appendChild(trail);
+
+  setTimeout(function () {
+    document.body.removeChild(trail);
+  }, 500);
+});
