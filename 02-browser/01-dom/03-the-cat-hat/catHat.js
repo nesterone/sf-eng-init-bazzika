a3522247868ad1a6dd79ae7f42a0b8715bd4f717
Artem Konovalov <artem.konovalov@globallogic.com>
2016-05-31 16:42:07 +0300

    Feat: Add solution for the CahHat Exercise

--- 02-browser/01-dom/03-the-cat-hat/catHat.js
+++ 02-browser/01-dom/03-the-cat-hat/catHat.js
@@ -1,1 +1,20 @@
-// Your code here.
+var section = document.querySelector('section');
+var cat = document.getElementById('cat');
+var hat = document.getElementById('hat');
+var angle = 0;
+var lastTime = null;
+
+section.style = 'margin: 20vw 0 0 40vw';
+
+function animate(time) {
+  if (lastTime !== null) {
+    angle += (time - lastTime) * 0.001;
+  }
+  lastTime = time;
+  cat.style.top = Math.sin(angle) * 50 + 'px';
+  cat.style.left = Math.cos(angle) * 200 + 'px';
+  hat.style.top = Math.cos(angle) * 200 + 'px';
+  hat.style.left = Math.sin(angle) * 550 + 'px';
+  requestAnimationFrame(animate);
+}
+requestAnimationFrame(animate);
