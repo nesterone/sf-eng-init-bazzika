6a8d00e263c7ea7d60a67aad79e9c3ccadb34491
bazzika <daryabelob@gmail.com>
2016-05-31 13:55:05 +0300

    write a simple tabbed interface

--- 02-browser/01-dom/06-tabs/asTabs.js
+++ 02-browser/01-dom/06-tabs/asTabs.js
@@ -1,5 +1,45 @@
-function asTabs() {
-  // Your code here.
+function asTabs(node) {
+  var arrayNodes = [];
+  var arrayButton = [];
+  var newButton;
+  var button;
+  var newText;
+  var i;
+  for (i = 0; i <= node.childNodes.length - 1; i++) {
+    if (node.childNodes[i].nodeType === document.ELEMENT_NODE) {
+      arrayNodes.push(node.childNodes[i]);
+    }
+  }
+  for (i = 0; i <= arrayNodes.length - 1; i++) {
+    newButton = document.createElement('button');
+    newText = document.createTextNode(arrayNodes[i].getAttribute('data-tabname'));
+    newButton.appendChild(newText);
+    document.body.appendChild(newButton);
+  }
+  button = document.body.getElementsByTagName('button');
+  for (i = 0; i <= button.length - 1; i++) {
+    arrayButton.push(button[i]);
+  }
+  arrayButton.forEach(function (element, j) {
+    element.addEventListener('click', function () {
+      var n;
+      var elements = [];
+      var newElement;
+      for (n = 0; n <= arrayButton.length - 1; n++) {
+        if (arrayButton[n].getAttribute('style') === 'background-color : red') {
+          arrayButton[n].removeAttribute('style');
+        }
+      }
+      element.setAttribute('style', 'background-color : red');
+      elements = document.getElementsByTagName('button');
+      if (elements[elements.length - 1].nextSibling !== null) {
+        elements[elements.length - 1].nextSibling.remove();
+      }
+      newElement = document.createElement('div');
+      newText = document.createTextNode(arrayNodes[j].textContent);
+      newElement.appendChild(newText);
+      document.body.appendChild(newElement);
+    });
+  });
 }
-
 asTabs(document.querySelector('#wrapper'));
