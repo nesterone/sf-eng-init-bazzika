48a491e000ec65efd84928515db738c8ad5bdd65
Artem Konovalov <artem.konovalov@globallogic.com>
2016-06-01 12:28:13 +0300

    Feat: Add solution for ElementByTagName Exercise

--- 02-browser/01-dom/02-elements-by-tag-name/byTagName.js
+++ 02-browser/01-dom/02-elements-by-tag-name/byTagName.js
@@ -1,7 +1,26 @@
 var paragraph = document.querySelector('p');
 
-function byTagName() {
-  // Your code here.
+function byTagName(node, tag) {
+  var tagCounter = [];
+
+  function innerTag(innerNode) {
+    var index;
+
+    if (innerNode.nodeType === document.ELEMENT_NODE) {
+      for (index = 0; index < innerNode.childNodes.length; index++) {
+        if (innerNode.childNodes[index].tagName === tag.toUpperCase()) {
+          tagCounter.push(innerNode.childNodes[index]);
+        }
+
+        if (innerNode.childNodes[index].nodeName !== Text) {
+          innerTag(innerNode.childNodes[index]);
+        }
+      }
+    }
+  }
+
+  innerTag(node);
+  return tagCounter;
 }
 
 console.log(byTagName(document.body, 'h1').length);
@@ -10,4 +29,3 @@ console.log(byTagName(document.body, 'span').length);
 // → 3
 console.log(byTagName(paragraph, 'span').length);
 // → 2
-
