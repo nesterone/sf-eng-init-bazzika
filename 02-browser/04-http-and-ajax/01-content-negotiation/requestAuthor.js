77884e6529b6d82f20b220571cdf2e2c18910855
DeuceX <braga.dmitriy@gmail.com>
2016-06-08 20:21:14 +0300

    Add requests implementation

--- 02-browser/04-http-and-ajax/01-content-negotiation/requestAuthor.js
+++ 02-browser/04-http-and-ajax/01-content-negotiation/requestAuthor.js
@@ -1,1 +1,13 @@
-// Your code here.
+function get(url, accept) {
+  var acceptHeader = accept || 'text/html';
+  var xhr = new XMLHttpRequest();
+  xhr.open('get', url, false);
+  xhr.setRequestHeader('Accept', acceptHeader);
+  xhr.send(null);
+  return xhr.responseText;
+}
+
+console.log(get('http://eloquentjavascript.net/author', 'text/plain'));
+console.log(get('http://eloquentjavascript.net/author', 'text/html'));
+console.log(get('http://eloquentjavascript.net/author', 'application/json'));
+console.log(get('http://eloquentjavascript.net/author', 'application/rainbows+unicorns'));
