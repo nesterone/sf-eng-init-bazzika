d270ab22cf65dce957dce6e9beebf9f3d7270b2f
bazzika <daryabelob@gmail.com>
2016-06-08 16:21:53 +0300

    Send requests to fetch all three formats

--- 02-browser/04-http-and-ajax/01-content-negotiation/requestAuthor.js
+++ 02-browser/04-http-and-ajax/01-content-negotiation/requestAuthor.js
@@ -1,1 +1,19 @@
-// Your code here.
+var types = ['text/plain',
+  'text/html',
+  'application/json',
+  'application/rainbow+unicorns'];
+function requestAuthor(type) {
+  var req = new XMLHttpRequest();
+  req.open('GET', 'http://eloquentjavascript.net/author', false);
+  req.setRequestHeader('accept', type);
+  req.send(null);
+  return req.responseText;
+}
+types.forEach(function (type) {
+  try {
+    console.log(type + ':\n', requestAuthor(type), '\n');
+  } catch (e) {
+    console.log('Request failed:  ' + e);
+  }
+});
+
