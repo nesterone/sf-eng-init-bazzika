40e33ff6ca940b54d4eb4e642023cbadc8e1e672
DeuceX <braga.dmitriy@gmail.com>
2016-06-10 10:48:10 +0300

    Add work bench implementation

--- 02-browser/05-forms/01-javascript-workbench/workbench.js
+++ 02-browser/05-forms/01-javascript-workbench/workbench.js
@@ -1,1 +1,16 @@
-// Your code here
+var button = document.querySelector('button');
+var output = document.querySelector('pre');
+var code = document.querySelector('textarea');
+
+button.addEventListener('click', function () {
+  var func;
+  var result;
+  try {
+    func = new Function('', code.value); // eslint-disable-line
+    result = func();
+  } catch (e) {
+    output.innerHTML = e.message;
+    return;
+  }
+  output.innerHTML = result;
+});
